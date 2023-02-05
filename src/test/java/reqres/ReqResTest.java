fb968bd61f4a8f240a10bb35d1d8827b0d78682b
Pavel Litvinenko <korvine1980@yandex.ru>
2023-02-05 12:01:17 +0500

    groovy

--- src/test/java/reqres/ReqResTest.java
+++ src/test/java/reqres/ReqResTest.java
@@ -4,13 +4,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import reqres.pojo.CreateUserRequestModel;
 import reqres.pojo.CreateUserResponseModel;
-import reqres.pojo.GetUserResponseModel;
 import reqres.pojo.LoginUserRequestModel;
 
 import static io.restassured.RestAssured.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 import static reqres.Specs.requestSpec;
 import static reqres.Specs.responseSpec;
 
@@ -23,36 +21,22 @@ public class ReqResTest {
     }
 
     @Test
-    void getUserTest() {
-       GetUserResponseModel response =
-                given()
-                        .get("/users/2")
-                        .then()
-                        .statusCode(200)
-                        .extract().as(GetUserResponseModel.class);
-
-        assertThat(response.getData().getId()).isEqualTo("2");
-        assertThat(response.getData().getEmail()).isEqualTo("janet.weaver@reqres.in");
-        assertThat(response.getData().getFirstName()).isEqualTo("Janet");
-        assertThat(response.getData().getLastName()).isEqualTo("Weaver");
-        assertThat(response.getData().getAvatar()).isEqualTo("https://reqres.in/img/faces/2-image.jpg");
-
+    void verifyQuantityUsersInResponse() {
+        int quantityUsersInResponse = 6;
+        given()
+                .get("/users/")
+                .then()
+                .body("data",
+                        hasSize(quantityUsersInResponse));
     }
 
     @Test
-    void successfulLoginUserTest() {
-        LoginUserRequestModel body = LoginUserRequestModel.builder()
-                .email("eve.holt@reqres.in")
-                .password("cityslicka")
-                .build();
-
+    void verifyEmailGroovyTest() {
         given()
-                .body(body)
-                .when()
-                .post("/login")
+                .get("/users/")
                 .then()
-                .statusCode(200)
-                .body("token", notNullValue());
+                .body("data.email.flatten()",
+                        hasItem("janet.weaver@reqres.in"));
     }
 
     @Test
